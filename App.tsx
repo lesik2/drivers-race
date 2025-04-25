6a96dc265ae919c95592182c5a3f6b1b47f2facb
Пухальский Алексей Юрьевич <APukhalskiy@alfabank.ru>
2025-04-25 13:43:01 +0300

    init: start project with react cli and set up basic configuration

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigation } from './src/navigation';
+
+
+function App(): React.JSX.Element {
+  return (
+    <Navigation />
+  );
+}
+
+
+
+export default App;
---
+++ src/navigation/index.tsx
@@ -0,0 +1,15 @@
+import {
+    View,
+    Text
+  } from 'react-native';
+import { API_URL } from '@env';
+
+export const Navigation = ()=>{
+    return (
+        <View>
+            <Text>
+                Hello World !!!!!
+            </Text>
+        </View>
+    )
+}
